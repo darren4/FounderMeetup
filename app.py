ee2a771399e3b5462d72f3e7dcf0e7ad7f9e3cd8
darren4 <darren.tas7@gmail.com>
2024-07-08 21:47:11 -0500

    app.py style

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
-from blueprints.main_blueprint import main_views
-from blueprints.auth_blueprint import auth_views
-from blueprints.professional_support_blueprint import professional_support_views
-from blueprints.peer_meetup_blueprint import peer_meetup_views
-
-from flask_login import LoginManager
-from models.database import user, User
-from bson import ObjectId
-from dotenv import dotenv_values
 import os
+
+from flask import Flask
+from flask_login import LoginManager
+from google.cloud.firestore_v1.base_document import DocumentSnapshot
+from google.cloud.firestore_v1.collection import CollectionReference
+from google.cloud.firestore_v1.document import DocumentReference
+
+from blueprints.auth_blueprint import auth_views
+from blueprints.main_blueprint import main_views
+from blueprints.peer_meetup_blueprint import peer_meetup_views
+from blueprints.professional_support_blueprint import professional_support_views
+from models.user import User
+from persistence.database_factory import DatabaseFactory
+
 
 # create an instance of the flask application
 app = Flask(__name__)
@@ -27,15 +31,12 @@ login.login_view = "/login"
 @login.user_loader  # 3
 def load_user(username):
     """Confirm user exists in database then use else return None"""
-    cur_user_ref = user.document(username)
-    cur_user = cur_user_ref.get()
+    users: CollectionReference = DatabaseFactory.build().get_users()
+    cur_user_ref: DocumentReference = users.document(username)
+    cur_user: DocumentSnapshot = cur_user_ref.get()
 
     if not cur_user.exists:
         return None
 
     # Create a user instance from the retrieved user
     return User(username)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
