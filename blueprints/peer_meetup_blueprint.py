35cf9c6cc12c063c4d318aca4ca6a02cfd3d7dc4
darren4 <darren.tas7@gmail.com>
2024-07-09 08:03:29 -0500

    cleaned up perr meeting

--- blueprints/peer_meetup_blueprint.py
+++ blueprints/peer_meetup_blueprint.py
@@ -1,11 +1,21 @@
-from flask import Blueprint, render_template, request, flash
-from flask_login import login_required, current_user
-from models.database import user_availability, user
-from typing import List, Final, Set, Dict, Any
-from bson.objectid import ObjectId
-from google.cloud.firestore_v1.base_query import FieldFilter
 from enum import Enum
+from typing import Final
+from typing import List
+
 from firebase_admin import firestore
+from flask import Blueprint
+from flask import flash
+from flask import render_template
+from flask import request
+from flask_login import current_user
+from flask_login import login_required
+from google.cloud.firestore_v1.base_document import DocumentSnapshot
+from google.cloud.firestore_v1.base_query import FieldFilter
+from google.cloud.firestore_v1.base_query import QueryType
+from google.cloud.firestore_v1.collection import CollectionReference
+
+from persistence.database_factory import DatabaseFactory
+from persistence.database_interface import DatabaseInterface
 
 
 peer_meetup_views = Blueprint("peer_meetup", __name__)
@@ -39,31 +49,42 @@ class UserAvailabilityFields(Enum):
 def peer_meetup():
     if request.method == "POST":
         current_username: str = current_user.username
+
+        database: DatabaseInterface = DatabaseFactory.build()
+        user_availability: CollectionReference = database.get_user_availability()
         user_availability.document(current_username).delete()
 
         availability: List[str] = [
             time for time in TIMES if request.form.get(time) == "on"
         ]
-        matches = user_availability.where(
-            filter=FieldFilter(
-                UserAvailabilityFields.AVAILABILITIES.value,
-                "array_contains_any",
-                availability,
+        matches: QueryType = (
+            user_availability.where(
+                filter=FieldFilter(
+                    UserAvailabilityFields.AVAILABILITIES.value,
+                    "array_contains_any",
+                    availability,
+                )
             )
-        ).order_by(UserAvailabilityFields.UPDATE_TIME.value).limit(1).get()
+            .order_by(UserAvailabilityFields.UPDATE_TIME.value)
+            .limit(1)
+            .get()
+        )
 
         if matches:
-            match = matches[0]
+            match: DocumentSnapshot = matches[0]
             match.reference.delete()
             matched_username = match.id
-            matched_email = user.document(matched_username).get().get("email")
+            users: CollectionReference = database.get_users()
+            matched_email: str = users.document(matched_username).get().get("email")
             flash(f"Matched with user with email {matched_email}")
             # TODO: send emails
         else:
-            user_availability.document(current_username).set({
-                UserAvailabilityFields.UPDATE_TIME.value: firestore.SERVER_TIMESTAMP,
-                UserAvailabilityFields.AVAILABILITIES.value: availability
-            })
+            user_availability.document(current_username).set(
+                {
+                    UserAvailabilityFields.UPDATE_TIME.value: firestore.SERVER_TIMESTAMP,
+                    UserAvailabilityFields.AVAILABILITIES.value: availability,
+                }
+            )
             flash("Times submitted", "info")
 
     return render_template("peer_meetup.html")
